05e1da0782e4c6072cee5b98735bf3debc18d673
Eli Conley <elijahtconley@gmail.com>
2022-09-15 18:08:31 -0400

    Updating file structure

--- CredPanda/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CredPanda.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
