ba541d2e88813e25b2a9a9306db93de033d3cb22
KronoDrake <0chronodrake1@gmail.com>
2022-09-23 13:11:38 -0400

    Unmapped picture and password from database columns
    Used another migration to unmapped user picture and password from admin accessible database

---
+++ CredPanda Financial Application/Data/Migrations/20220923162442_unmappedUserPictureAndPass.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CredPanda_Financial_Application.Data.Migrations
+{
+    public partial class unmappedUserPictureAndPass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "picture",
+                table: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "picture",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
