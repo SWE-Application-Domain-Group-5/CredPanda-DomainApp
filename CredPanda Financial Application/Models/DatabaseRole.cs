05b4e5b1a615e59841ba46e24ce08a1141845a8b
Eli Conley <elijahtconley@gmail.com>
2022-09-25 22:56:32 -0400

    Updated to EliApp
    The BIG update to implement many features such as role management, user management, and more

--- CredPanda Financial Application/Models/DatabaseRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CredPanda_Financial_Application.Models
-{
-    public class DatabaseRole
-    {
-        public int Id { get; set; }
-        public string roleName { get; set; }
-    }
-}
---
+++ EliApp/EliApp/Areas/Identity/Data/EliAppUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EliApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the EliAppUser class
+public class EliAppUser : IdentityUser
+{
+    //Get user's first name
+    [PersonalData]
+    public string? FirstName { get; set; }
+    //Get user's last name
+    [PersonalData]
+    public string? LastName { get; set; }
+    //Get user's address
+    [PersonalData]
+    public string? Address { get; set; }
+    //Get user's Date of birth
+    [PersonalData]
+    public DateTime DOB { get; set; }
+    [PersonalData]
+    public Nullable<byte[]> ProfilePicture { get; set; }
+    [PersonalData]
+    //Get user's date of registration
+    public DateTime RegisterDate { get; set; }
+    //Get user's generated username
+    [PersonalData]
+    public string? GeneratedUserName { get; set; }
+    /*
+    //Get user's activation state
+    [PersonalData]
+    public bool? isActive { get; set; }
+    //Get user's List of old passwords
+    [PersonalData]
+    public List<string>? oldPasswords { get; set; } 
+    */
+}
+
+//This is basically a way to add extra attributes to IdentityUser - Eli
