46bb75824a9ae397faefc8788044703dab5121a4
Eli Conley <elijahtconley@gmail.com>
2022-09-27 19:34:55 -0400

    Hopefully EliApp now

---
+++ EliApp/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using EliApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using EliApp.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EliApp.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class UserController : Controller
+    {
+        UserManager<EliAppUser> userManager;
+
+        public UserController(UserManager<EliAppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = userManager.Users.ToList();
+            return View(users);
+        }
+    }
+
+    // THIS IS WHERE WE SHOULD IMPLEMENT DEACTIVATE/ACTIVATE USERS AND EMAIL USERS
+}
---
+++ EliApp/Migrations/20220926203358_IsActiveAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EliApp.Migrations
+{
+    public partial class IsActiveAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isActive",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isActive",
+                table: "AspNetUsers");
+        }
+    }
+}
