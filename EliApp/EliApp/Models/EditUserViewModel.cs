1ec12f671acbf62f98ea961d9e4dbc99d751d659
KronoDrake <0chronodrake1@gmail.com>
2022-10-06 19:03:11 -0400

    Revert "Delete .vs/EliApp/DesignTimeBuild directory"
    This reverts commit 9af0b50bb3e4db3463b831cd5390856311fe96f4.

---
+++ EliApp/EliApp/Models/EditUserViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EliApp.Models
+{
+	public class EditUserViewModel
+	{
+        //Add attributes here to add them to the configuration list
+		public EditUserViewModel()
+		{
+            Roles = new List<string>();
+        }
+        
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime? DOB { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public bool isActive{ get; set; } 
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        
+
+        public IList<string> Roles { get; set; }
+    }
+}
