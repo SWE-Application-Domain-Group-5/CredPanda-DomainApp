1268b305bb5b83ed3925ec6c157bd42a1ebcce92
Eli Conley <elijahtconley@gmail.com>
2022-11-05 19:37:04 -0400

    Refined Account Creation
    Account number and date of creation now auto populate and current value of account is set to the initial value

--- EliApp/Models/AccountModel.cs
+++ EliApp/Models/AccountModel.cs
@@ -1,20 +1,51 @@
-﻿namespace EliApp.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.Security.Cryptography;
+
+namespace EliApp.Models
 {
     public class AccountModel
     {
+        public AccountModel()
+        {
+            DateTime now = new DateTime();
+            now = DateTime.Now;
+            Random rand = new Random();
+            string accnum;
+            accnum = now.Month.ToString() + now.Day.ToString();
+            for (int ctr = 0; ctr <= 5; ctr++)
+            {
+                accnum += rand.Next(0, 9).ToString();
+            }
+            AccountNumber = accnum;
+            AccountCurrentBalance = AccountInitialBalance;
+        }
         public int Id { get; set; }
+        [DisplayName("Name")]
         public string AccountName { get; set; }
-        public int AccountNumber { get; set; }
+        [DisplayName("Account #")]
+        public string AccountNumber { get; set; }
+        [DisplayName("Description")]
         public string AccountDescription { get; set; }
+        [DisplayName("Type")]
         public string AccountType { get; set; } //debit or credit
+        [DisplayName("Category")]
         public string AccountCategory { get; set; }
+        [DisplayName("Subcategory")]
         public string AccountSubcategory { get; set; }
+        [DisplayName("Initial Balance")]
         public float AccountInitialBalance { get; set; }
+        [DisplayName("Current Balance")]
         public float AccountCurrentBalance { get; set; }
-        public DateTime AccountCreationTime { get; set; }
+        [DisplayName("Creation Time")]
+        public DateTime AccountCreationTime { get; set; } = DateTime.Now;
+        [DisplayName("UserID")]
         public string AccountUserID { get; set; }
+        [DisplayName("Order")]
         public string AccountOrder { get; set; } //"essentially the order the entries are made i.e. Cash (01), Credit (02). "
+        [DisplayName("Statement")]
         public string AccountStatement { get; set; }
+        [DisplayName("Comment")]
         public string AccountComment { get; set; }
     }
 }
