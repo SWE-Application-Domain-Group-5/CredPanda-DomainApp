54c4abe0eb57bb09780b9be6e661fe80322c0cde
Eli Conley <elijahtconley@gmail.com>
2022-12-04 23:00:22 -0500

    Changed fonts to black

---
+++ EliApp/Migrations/20221205035234_PasswordExpirationEli.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EliApp.Migrations
+{
+    public partial class PasswordExpirationEli : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "expireDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "passwordChangedDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expireDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "passwordChangedDate",
+                table: "AspNetUsers");
+        }
+    }
+}
